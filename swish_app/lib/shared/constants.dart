2c2e72421f5b645e0939aa60f1a0ab1eae33fba0
bsingdep <singdepbenjamin@gmail.com>
2023-09-13 23:19:20 -0700

    swish

---
+++ swish_app/lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:657605525221:web:9d31ed53b4c19dd27e9e0c";
+  static String apiKey = "AIzaSyB_PT4c6KosJPFOWy_49KzfAdvTB2mjIxg";
+  static String messagingSenderId = "657605525221";
+  static String projectId = "encrytionethree";
+  final primaryColor = const Color(0xFFee7b64);
+}
